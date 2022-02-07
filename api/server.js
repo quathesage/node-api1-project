26794f6df9b0c2bb583f654f8d8832195da4dc8c
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2022-02-07 14:31:00 -0600

    Post Request

--- api/server.js
+++ api/server.js
@@ -1,6 +1,26 @@
 const express = require("express")
 const server = express()
+const Users = require("./users/model")
 
 server.use(express.json())
+
+server.post("/api/users", async (req, res) => {
+  try {
+    const { name, bio } = req.body
+    if (!name || !bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" })
+    } else {
+      const newUser = await Users.insert({ name, bio })
+      console.log(newUser)
+      res.json(newUser)
+    }
+  } catch {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    })
+  }
+})
 
 module.exports = server
